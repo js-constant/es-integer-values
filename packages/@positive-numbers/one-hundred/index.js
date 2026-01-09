2e8d2c06904fc05848b98a16855ca35110c9e5a6
tj-commits <thefoxjones@gmail.com>
2026-01-09 14:57:06 -0600

    one hundred

--- packages/@positive-numbers/one-hundred/index.js
+++ packages/@positive-numbers/one-hundred/index.js
@@ -1,4 +1,2 @@
-const five = require("five")
-
-const number = ((five() + five()) * (five() + five()))
+const number = require("number-one-hundred") // Import the number 100 from the dependency
 module.exports = number // Export the number 100
