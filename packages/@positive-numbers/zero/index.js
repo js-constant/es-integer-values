9a675633fe6736eecaadbf19fda6c70222406aa1
tj-commits <thefoxjones@gmail.com>
2026-01-11 09:50:34 -0600

    0 update

--- packages/@positive-numbers/zero/index.js
+++ packages/@positive-numbers/zero/index.js
@@ -1,2 +1,6 @@
-const dependency = require("number-zero");
-module.exports = dependency // Export the number 0
+const dependency = require("zerosurge");
+const falseValue = require("false-value")
+module.exports = dependency.returnZero({
+  loggingEnabled: falseValue,
+  method: dependency.ZeroCalculationMethod.RandomNumberSelfSubtraction
+}) // Export the number 0
