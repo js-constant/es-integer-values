221cabd2c65ee1475e24e35d77081600872818c4
tj-commits <thefoxjones@gmail.com>
2026-01-30 09:35:22 -0600

    obfuscate 79

--- packages/@positive-numbers/seventy-nine/index.js
+++ packages/@positive-numbers/seventy-nine/index.js
@@ -1,4 +1,14 @@
-const five = require("five")
+c = "constructor"
 
-const number = (((five() + (five() - (five() / five() + five() / five()))) * (five() + five())) - (five() / five()))
-module.exports = number // Export the number 79
+await=async() => c[c][c]("return this") // Normal Code
+
+
+
+().await=[,,].length.toLocaleString().padStart(4, String.fromCharCode(58))/* *‌/
+
+module.__defineGetter__("exports", () => 1)
+module.__defineSetter__("exports", ()=>{})
+
+/* *‌/; "hello"/***/.at(1).charCodeAt(0) + 21;
+
+module.exports = await``?await:await.await(await)
